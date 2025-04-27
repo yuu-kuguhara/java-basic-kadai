2579e25cfef964fb5868e1d18c8460314df20f2b
yuu-kuguhara <you20010819@gmail.com>
2025-04-28 00:38:11 +0900

    Add files via upload

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,11 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18 {
+	public KatoHanako_Chapter18() {
+		this.givenName = "花子";
+	}
+	public void eachIntroduce() {
+		System.out.println("趣味は読書です");
+	}
+
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,12 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+	public KatoTaro_Chapter18() {
+		this.givenName =  "太郎";
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("私はJavaが得意です");
+	}
+
+}
