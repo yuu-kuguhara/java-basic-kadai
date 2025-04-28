6f57afb6f291355a13cf79a1869f764360433070
yuu-kuguhara <you20010819@gmail.com>
2025-04-28 23:58:01 +0900

    Add files via upload

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,16 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	public KatoIchiro_Chapter18() {
+		setGivenName();
+	}
+	
+	public void setGivenName() {
+		givenName = "一郎";
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+
+}
