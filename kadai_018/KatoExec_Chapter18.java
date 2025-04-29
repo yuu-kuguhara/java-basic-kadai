ac3fac97985c2402f3f57107aa0265e32faa9592
yuu-kuguhara <you20010819@gmail.com>
2025-04-29 23:38:27 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,22 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName();
+		
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName();
+		
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		hanako.setGivenName();
+		
+		taro.execIntroduce();
+		ichiro.execIntroduce();
+		hanako.execIntroduce();
+
+	}
+
+}
---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,12 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18 {
+	public void setGivenName() {
+		givenName = "一郎";
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+
+}
